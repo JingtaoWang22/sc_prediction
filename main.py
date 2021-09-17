c9f5d607f3dfd2416cb6eb71317a9f9a6544dc89
Jingtao <jingtao2@andrew.cmu.edu>
2021-09-17 17:38:57 -0400

    add initial VAE model, add training pipeline

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ import os
 from utils import *
 import numpy as np
 import torch
+from models import *
 
-
-
+from sklearn.cluster import KMeans
 
 
 
@@ -35,7 +35,8 @@ raw_dataset_folder = '../../cellar_data'
 '''
 HYPER PARAMETERS
 '''
-
+PATIENT_BATCH = 1
+N_PATIENT_CLUSTERS = 2
 
 
 
@@ -44,3 +45,40 @@ if __name__ == "__main__":
 
     # create dataset
     b,sc,genes,datanames  = create_dataset_folder(raw_dataset_folder)
+    
+    
+    
+    cluster_label = []
+    modellist = []
+    have_sc=[]
+    '''
+    for i in range(len(datanames)):
+        model = initialize model
+        modellist.append(model)
+    '''
+    
+    # one iteration
+    for iteration in range(2): # the iterations allowed by the budget given
+        if (iteration == 0):
+            #do clustering and testing in the first iteration
+            kmeans = KMeans(n_clusters=N_PATIENT_CLUSTERS, random_state=0).fit(np.array(b))
+            cluster_label = kmeans.labels_
+            have_sc.append(0)
+            
+            
+        
+        for i in range(len(datanames)): # for each individual
+            print("Trainign VAE for " + (i+1) + "th patient.")
+            x = sc[i]
+            bulk = b[i]
+            genenames = genes[i]
+        
+            # fit the model if has sc
+
+        ## choose the next batch of patients
+
+
+# output simulations and errors
+
+
+
